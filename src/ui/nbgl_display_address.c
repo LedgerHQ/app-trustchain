844d78986d1ba2b5c59bd00d78039b593db93c77
Pierre Pollastri <pierre@ledger.fr>
2023-09-06 10:29:59 +0200

    ci: Pull submodules for unit-tests

--- src/ui/nbgl_display_address.c
+++ src/ui/nbgl_display_address.c
@@ -34,64 +34,10 @@
 #include "constants.h"
 #include "../globals.h"
 #include "../sw.h"
-#include "../address.h"
 #include "action/validate.h"
-#include "../transaction/types.h"
 #include "../menu.h"
 
-static char g_address[43];
-
-static void confirm_address_rejection(void) {
-    // display a status page and go back to main
-    validate_pubkey(false);
-    nbgl_useCaseStatus("Address verification\ncancelled", false, ui_menu_main);
-}
-
-static void confirm_address_approval(void) {
-    // display a success status page and go back to main
-    validate_pubkey(true);
-    nbgl_useCaseStatus("ADDRESS\nVERIFIED", true, ui_menu_main);
-}
-
-static void review_choice(bool confirm) {
-    if (confirm) {
-        confirm_address_approval();
-    } else {
-        confirm_address_rejection();
-    }
-}
-
-static void continue_review(void) {
-    nbgl_useCaseAddressConfirmation(g_address, review_choice);
-}
-
 int ui_display_address() {
-    if (G_context.req_type != CONFIRM_ADDRESS || G_context.state != STATE_NONE) {
-        G_context.state = STATE_NONE;
-        return io_send_sw(SW_BAD_STATE);
-    }
-
-    char bip32_path[60] = {0};
-    if (!bip32_path_format(G_context.bip32_path,
-                           G_context.bip32_path_len,
-                           bip32_path,
-                           sizeof(bip32_path))) {
-        return io_send_sw(SW_DISPLAY_BIP32_PATH_FAIL);
-    }
-
-    memset(g_address, 0, sizeof(g_address));
-    uint8_t address[ADDRESS_LEN] = {0};
-    if (!address_from_pubkey(G_context.pk_info.raw_public_key, address, sizeof(address))) {
-        return io_send_sw(SW_DISPLAY_ADDRESS_FAIL);
-    }
-    snprintf(g_address, sizeof(g_address), "0x%.*H", sizeof(address), address);
-
-    nbgl_useCaseReviewStart(&C_app_boilerplate_64px,
-                            "Verify BOL address",
-                            NULL,
-                            "Cancel",
-                            continue_review,
-                            confirm_address_rejection);
     return 0;
 }
 

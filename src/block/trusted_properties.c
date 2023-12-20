e7121478bd21d17911c567c6d498c1b9f69e4c66
Sarah GLINER <sarah.gliner@ledger.fr>
2023-12-20 15:42:10 +0100

    clean: remove code and minor tweaks

--- src/block/trusted_properties.c
+++ src/block/trusted_properties.c
@@ -30,27 +30,15 @@ int deserialize_trusted_member(uint8_t *buffer, size_t buffer_size, stream_trust
     if (buffer_size < TP_BUFFER_SIZE_NEW_MEMBER) {
         return TP_BUFFER_OVERFLOW;
     }
+
     crypto_digest(buffer, TP_NONCE_SIZE + sizeof(stream_trusted_member_t), hash, sizeof(hash));
+
     if (memcmp(hash, buffer + TP_NONCE_SIZE + sizeof(stream_trusted_member_t), TP_CHECKSUM_LEN) !=
         0) {
         return TP_INVALID_CHECKSUM;
     }
-    memcpy((void *) out, buffer + TP_NONCE_SIZE, sizeof(stream_trusted_member_t));
-    return TP_SUCCESS;
-}
 
-int set_trusted_member(stream_trusted_member_t *member, uint8_t *buffer, size_t buffer_size) {
-    memcpy(&G_context.stream.trusted_member, member, sizeof(stream_trusted_member_t));
-    if (buffer != NULL) {
-        return serialize_trusted_member(member, buffer, buffer_size);
-    }
-    return TP_SUCCESS;
-}
+    memcpy(out, buffer + TP_NONCE_SIZE, sizeof(stream_trusted_member_t));
 
-int read_and_set_trusted_member(uint8_t *buffer, size_t buffer_size, stream_trusted_member_t *out) {
-    int ret = deserialize_trusted_member(buffer, buffer_size, out);
-    if (ret == TP_SUCCESS) {
-        memcpy(&G_context.stream.trusted_member, out, sizeof(stream_trusted_member_t));
-    }
-    return ret;
+    return TP_SUCCESS;
 }
--- src/block/trusted_properties.h
+++ src/block/trusted_properties.h
@@ -1,5 +1,12 @@
 #pragma once
 
+#include "../stream/stream.h"
+
+#define TP_CHECKSUM_LEN 4
+#define TP_NONCE_SIZE   4
+#define TP_BUFFER_SIZE_NEW_MEMBER \
+    (TP_NONCE_SIZE + sizeof(stream_trusted_member_t) + TP_CHECKSUM_LEN)
+
 typedef enum {
     TP_IV = 0x00,
     TP_ISSUER_PUBLIC_KEY = 0x01,
@@ -8,7 +15,7 @@ typedef enum {
     TP_COMMAND_IV = 0x04,
     TP_GROUPKEY = 0x05,
     TP_NEW_MEMBER = 0x06
-} tp_type_t;
+} tp_type_e;
 
 typedef enum {
     TP_SUCCESS = 0x00,
@@ -16,39 +23,7 @@ typedef enum {
     TP_BUFFER_OVERFLOW = -2,
     TP_FAILED_TO_HASH = -3,
     TP_INVALID_CHECKSUM = -4,
-} tp_error_t;
-
-#include "types.h"
-#include "../stream/stream.h"
-#include "../constants.h"
-
-#define TP_CHECKSUM_LEN 4
-#define TP_NONCE_SIZE   4
-#define TP_BUFFER_SIZE_NEW_MEMBER \
-    (TP_NONCE_SIZE + sizeof(stream_trusted_member_t) + TP_CHECKSUM_LEN)
+} tp_error_e;
 
 int serialize_trusted_member(stream_trusted_member_t *member, uint8_t *buffer, size_t buffer_size);
 int deserialize_trusted_member(uint8_t *buffer, size_t buffer_size, stream_trusted_member_t *out);
-
-/**
- * Set the trusted member in global context and serialize it in the given buffer
- *
- * @param[in] member The trusted member to set
- * @param[out] buffer The buffer to serialize the trusted member in (NULL to only set the global
- * context)
- * @param[in] buffer_size The size of the buffer
- *
- * @return The length of the serialized trusted member or an error code
- */
-int set_trusted_member(stream_trusted_member_t *member, uint8_t *buffer, size_t buffer_size);
-
-/**
- * Read the trusted member from the given buffer and set it in global context.
- *
- * @param[in] buffer The buffer to read the trusted member from
- * @param[in] buffer_size The size of the buffer
- * @param[out] out The trusted member to set (NULL to only set the global context)
- *
- * @return The length of the serialized trusted member or an error code
- */
-int read_and_set_trusted_member(uint8_t *buffer, size_t buffer_size, stream_trusted_member_t *out);

1d115b3dd1b49960dfa30a684c1fbcf53aa3a0bb
Sarah GLINER <sarah.gliner@ledger.fr>
2023-12-22 15:52:08 +0100

    SeedID: implement feature

---
+++ src/handler/get_seed_id.c
@@ -0,0 +1,70 @@
+/*****************************************************************************
+ *   Ledger App Trustchain.
+ *   (c) 2023 Ledger SAS.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *****************************************************************************/
+
+#include <stdint.h>   // uint*_t
+#include <stdbool.h>  // bool
+#include <stddef.h>   // size_t
+#include <string.h>   // memset, explicit_bzero
+
+#include "os.h"
+#include "cx.h"
+
+#include "../globals.h"
+#include "../types.h"
+#include "io.h"
+#include "../sw.h"
+#include "../crypto.h"
+#include "buffer.h"
+#include "../ui/display.h"
+#include "../helper/send_response.h"
+#include "../constants.h"
+#include "challenge_parser.h"
+#include "challenge_sign.h"
+
+static challenge_ctx_t challenge_ctx;
+
+int handler_get_seed_id(buffer_t* buffer) {
+    int error;
+    uint8_t challenge_hash[CX_SHA256_SIZE];
+    LEDGER_ASSERT(buffer != NULL, "Null pointer");
+
+    PRINTF("Parsing buffer\n");
+
+    error = challenge_parse_buffer(buffer, &challenge_ctx);
+    if (error) {
+        PRINTF("Error parsing buffer: %d \n", error);
+        return io_send_sw(error);
+    }
+
+    crypto_digest(buffer->ptr, buffer->size, challenge_hash, CX_SHA256_SIZE);
+
+    error = verify_challenge_signature(&challenge_ctx, challenge_hash);
+    if (error) {
+        PRINTF("Error verifying challenge: %d \n", error);
+#warning TODO
+        // return io_send_sw(error);
+    }
+
+    // TODO :  error = display_authentification_request();
+
+    error = sign_challenge(challenge_hash);
+    if (error) {
+        return io_send_sw(error);
+    }
+
+    return 0;
+}
